6879c5c222a6f62d6cda6f02f0a63d26c4b80bfa
tsukasa-kiyofuji <tsukasa.wdpro@gmail.com>
2023-10-30 06:26:10 +0900

    s

--- signup.php
+++ signup.php
@@ -7,16 +7,21 @@
     exit();
   }
 
-  $dsn = "mysql:dbname=t1thivzsrdk5y4wj;host=nnsgluut5mye50or.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-  $user = "xzwbwzes9yv8yjbb";
-  $password = "vkmg1w9wizgqg3pn";
+$host = getenv("nnsgluut5mye50or.cbetxkdyhwsb.us-east-1.rds.amazonaws.com ");
+$username = getenv("xzwbwzes9yv8yjbb");
+$password = getenv("vkmg1w9wizgqg3pn");
+$database = getenv("t1thivzsrdk5y4wj");
+$port = getenv("3306");
 
-  try {
-    $pdo = new PDO($dsn, $user, $password);  
-  } catch (Exception $e) {
-    print("Connection failed:".$e->getMessage());
-    exit();
-  }
+try {
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$database", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo "データベースに接続しました";
+
+  } catch (PDOException $e) {
+    die("データベースエラー: " . $e->getMessage());
+}
 ?>
 
 <!DOCTYPE html>
