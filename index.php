fd8ad2acd350cbc74adf3fc7ce04afc11400db31
tsukasa-kiyofuji <tsukasa.wdpro@gmail.com>
2024-01-06 19:10:29 +0900

    kkk

--- index.php
+++ index.php
@@ -99,14 +99,3 @@
 </body>
 
 <?php $pdo = null ?>
-
-  $dsn = "mysql:dbname=t1thivzsrdk5y4wj;host=nnsgluut5mye50or.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-  $user = "xzwbwzes9yv8yjbb";
-  $password = "vkmg1w9wizgqg3pn";
-
-  try {
-    $pdo = new PDO($dsn, $user, $password);  
-  } catch (Exception $e) {
-    print("Connection failed:".$e->getMessage());
-    exit();
-  }
